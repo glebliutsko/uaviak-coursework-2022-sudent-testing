030a27460a6b4133a5bfbae57d256dadb81f14e3
Gleb Liutsko <gleb@liutsko.ru>
2022-02-05 21:23:09 +0300

    Rename StudentTesting --> StudentTesting.Application

---
+++ StudentTesting/StudentTesting.MaterialDesign/AttachedProperty/HeaderGroupBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace StudentTesting.MaterialDesign.AttachedProperty
+{
+    public static class HeaderGroupBox
+    {
+        #region HeaderAligment
+        public static HorizontalAlignment GetHeaderAligment(DependencyObject obj) =>
+            (HorizontalAlignment)obj.GetValue(HeaderAligmentProperty);
+
+        public static void SetHeaderAligment(DependencyObject obj, HorizontalAlignment value) =>
+            obj.SetValue(HeaderAligmentProperty, value);
+
+        public static readonly DependencyProperty HeaderAligmentProperty =
+            DependencyProperty.RegisterAttached("HeaderAligment", typeof(HorizontalAlignment), typeof(HeaderGroupBox), new PropertyMetadata(HorizontalAlignment.Left));
+        #endregion
+
+        #region HeaderFontSize
+        public static double GetHeaderFontSize(DependencyObject obj) =>
+            (double)obj.GetValue(HeaderFontSizeProperty);
+
+        public static void SetHeaderFontSize(DependencyObject obj, double value) =>
+            obj.SetValue(HeaderFontSizeProperty, value);
+
+        // Using a DependencyProperty as the backing store for HeaderFontSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderFontSizeProperty =
+            DependencyProperty.RegisterAttached("HeaderFontSize", typeof(double), typeof(HeaderGroupBox), new PropertyMetadata(16.0));
+        #endregion
+    }
+}
---
+++ StudentTesting/StudentTesting.MaterialDesign/AttachedProperty/Hint.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace StudentTesting.MaterialDesign.AttachedProperty
+{
+    public static class Hint
+    {
+        #region HintText
+        public static readonly DependencyProperty HintTextProperty =
+               DependencyProperty.RegisterAttached("HintText", typeof(string), typeof(Hint), new PropertyMetadata(""));
+
+        public static string GetHintText(DependencyObject obj) =>
+            (string)obj.GetValue(HintTextProperty);
+        public static void SetHintText(DependencyObject obj, string value) =>
+            obj.SetValue(HintTextProperty, value);
+        #endregion
+    }
+}
