51a1b85c4802aaa2947b3eeadba331c5f2236b4e
Gleb Liutsko <gleb@liutsko.ru>
2022-02-11 16:22:56 +0400

    Добавлена конфигурация для RabComp

--- src/StudentTesting.Application/Configuration.cs
+++ src/StudentTesting.Application/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace StudentTesting.Application
 {
+    // Win10Virt
     static class Configuration
     {
         public const string ADDRESS_DB = "10.0.0.2";
@@ -9,4 +10,15 @@
         public const string USER_DB = "sa";
         public const string PASSWORD_DB = "R1409p1209";
     }
+
+    // RabComp
+    /*static class Configuration
+    {
+        public const string ADDRESS_DB = @"localhost\SQLEXPRESS";
+        public const string DATABASE = "StudentTesting";
+
+        public const bool INTEGRATED_SECURITY = true;
+        public const string USER_DB = "";
+        public const string PASSWORD_DB = "";
+    }*/
 }
