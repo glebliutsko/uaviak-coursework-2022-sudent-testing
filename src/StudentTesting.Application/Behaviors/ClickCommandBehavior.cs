7fc028ec6761611187a31e453907c94e168f5309
Gleb <gleb@liutsko.ru>
2022-05-20 11:25:46 +0400

    ClickBehavior

---
+++ src/StudentTesting.Application/Behaviors/ClickCommandBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StudentTesting.Application.Behaviors
+{
+    public class ClickCommandBehavior : Behavior<UIElement>
+    {
+        #region Command
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(ClickCommandBehavior), new PropertyMetadata(null));
+        #endregion
+
+        #region CommandParameter
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(ClickCommandBehavior), new PropertyMetadata(null));
+        #endregion
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.MouseUp += MouseUp;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.MouseUp -= MouseUp;
+        }
+
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (Command?.CanExecute(CommandParameter) == true)
+                Command.Execute(CommandParameter);
+        }
+    }
+}
--- src/StudentTesting.Application/Views/Course/CourseItemControlElement.xaml.cs
+++ src/StudentTesting.Application/Views/Course/CourseItemControlElement.xaml.cs
@@ -9,35 +9,9 @@ namespace StudentTesting.Application.Views.Course
     /// </summary>
     public partial class CourseItemControlElement : UserControl
     {
-        #region ClickCommand
-        public ICommand ClickCommand
-        {
-            get { return (ICommand)GetValue(ClickCommandProperty); }
-            set { SetValue(ClickCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty ClickCommandProperty =
-            DependencyProperty.Register("ClickCommand", typeof(ICommand), typeof(CourseItemControlElement), new PropertyMetadata(null));
-
-        public object ClickCommandParameter
-        {
-            get { return (object)GetValue(ClickCommandParameterProperty); }
-            set { SetValue(ClickCommandParameterProperty, value); }
-        }
-
-        public static readonly DependencyProperty ClickCommandParameterProperty =
-            DependencyProperty.Register("ClickCommandParameter", typeof(object), typeof(CourseItemControlElement), new PropertyMetadata(null));
-        #endregion
-
         public CourseItemControlElement()
         {
             InitializeComponent();
-        }
-
-        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (ClickCommand?.CanExecute(ClickCommandParameter) == true)
-                ClickCommand.Execute(ClickCommandParameter);
         }
     }
 }
